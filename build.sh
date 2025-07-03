eca7f5668dac25d4f17facd85b5e59d028b3c7b0
unknown <rolando@email.com>
2025-07-03 15:50:27 -0500

    crear usario admin

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ if [ "$CREATE_SUPERUSER" = "true" ]; then
   python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-username = "$ADMIN_USERNAME"
 email = "$ADMIN_EMAIL"
 password = "$ADMIN_PASSWORD"
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password)
 END
 fi
+
+
 
