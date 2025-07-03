69b8fba6f450722dabdfbb2a55252dbb0700ffe2
unknown <rolando@email.com>
2025-07-03 15:45:17 -0500

    creando el superusuario

--- build.sh
+++ build.sh
@@ -4,8 +4,14 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 if [ "$CREATE_SUPERUSER" = "true" ]; then
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-  username = '$ADMIN_USERNAME'; email = '$ADMIN_EMAIL'; password = '$ADMIN_PASSWORD'; \
-  if not User.objects.filter(username=username).exists(): \
-      User.objects.create_superuser(username, email, password)" | python manage.py shell
+  python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = "$ADMIN_USERNAME"
+email = "$ADMIN_EMAIL"
+password = "$ADMIN_PASSWORD"
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+END
 fi
+
