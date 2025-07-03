7de9e5c31f8c0bd560aa92975e0fb53a08561b8b
unknown <rolando@email.com>
2025-07-03 15:40:16 -0500

    creando el superusuario

--- build.sh
+++ build.sh
@@ -2,3 +2,10 @@ set -o errexist
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
+  username = '$ADMIN_USERNAME'; email = '$ADMIN_EMAIL'; password = '$ADMIN_PASSWORD'; \
+  if not User.objects.filter(username=username).exists(): \
+      User.objects.create_superuser(username, email, password)" | python manage.py shell
+fi
