3413b5453a9f56f4c28731ca9211ce48facc6393
unknown <rolando@email.com>
2025-07-05 14:51:48 -0500

    Corrige admin sala

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -29,7 +29,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # ========================
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your-secret-key') 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
